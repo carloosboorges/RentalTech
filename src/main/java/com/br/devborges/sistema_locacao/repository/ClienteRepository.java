e87431a13d0f36db36469283556266118e116a24
Carlos Borges <carloos-boorges@hotmail.com>
2025-11-19 13:57:41 -0300

    feat(repository): adiciona repositórios base para entidades do sistema de locação

---
+++ src/main/java/com/br/devborges/sistema_locacao/repository/ClienteRepository.java
@@ -0,0 +1,6 @@
+package com.br.devborges.sistema_locacao.repository;
+import com.br.devborges.sistema_locacao.entity.ClienteModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
+}
